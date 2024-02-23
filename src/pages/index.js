51dab118a5cce066be26792c8502bcaf9123bde9
Jade Rubick <jade@rubick.com>
2024-02-23 14:19:23 -0800

    Add Sarah Madden's thank you to website

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Seo from "../components/Seo";
 import { FaUserPlus, FaRss, FaMastodon, FaLinkedin } from "react-icons/fa";
 import charity from "../images/png/charity.png";
 import rachel from "../images/png/rachel.png";
-
+import sarah from "../images/png/sarah.png";
 
 const IndexPage = props => {
 
@@ -65,6 +65,7 @@ const IndexPage = props => {
         <p></p>
 
         <img src={charity} className="charity" width="100%" />
+        <img src={sarah} className="charity" width="100%" />
         <img src={rachel} className="charity" width="65%" />
 
         <p></p>
