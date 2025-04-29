8900589f4977102dad787b8a50014109ff1017a0
Jade Rubick <jade@rubick.com>
2025-04-29 13:30:14 -0700

    fix: Add missing GatsbyImage import in ReImg component

--- src/components/Article/ReImg.js
+++ src/components/Article/ReImg.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { GatsbyImage } from "gatsby-plugin-image";
 import theme from "../../theme/theme.yaml";
 
 // Helper to convert old fluid format to a compatible format for rendering
